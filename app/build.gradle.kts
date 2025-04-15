09a94bc7a0a8ca17147274c82e724aa456c7d852
msjeong <wnfhcm1@hecto.co.kr>
2025-04-16 08:13:44 +0900

    [vcs] 1.0.0 재배포 - API 관련 추가 개발

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "kr.co.rolling.moment"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24041100
+        versionCode = 24041600
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
